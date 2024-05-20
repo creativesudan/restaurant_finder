70d97382e1b07e6d4848b37304da6a0f00af0575
creativesudan <vinaysudan@vinays-Air.hgu_lan>
2024-05-21 01:59:39 +0530

    initial

---
+++ src/main/java/Item.java
@@ -0,0 +1,53 @@
+//public class Item {
+//    private String name;
+//    private int price;
+//
+//    public Item(String name, int price) {
+//        this.name = name;
+//        this.price = price;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//    @Override
+//    public String toString(){
+//        return  name + ":"
+//                + price
+//                + "\n"
+//                ;
+//    }
+//}
+
+
+
+
+public class Item {
+    private String name;
+    private int price;
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": $" + price;
+    }
+}
+
+//public class itemNotFoundException extends Exception {
+//    public itemNotFoundException(String itemName) {
+//        super("Item not found: " + itemName);
+//    }
+//}
+
